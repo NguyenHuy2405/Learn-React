5a2b395cb0af69a799118464384a236af8d3f481
Huy2405 <nguyenquochuy1122017@gmail.com>
2025-01-13 22:27:19 +0700

    bai 26

--- src/components/MyComponent.js
+++ src/components/MyComponent.js
@@ -1,12 +1,29 @@
 import React from "react";
+import AddUserInfor from "./AddUserInfor";
+import DisplayInfor from "./DisplayInfor";
 
 class MyComponent extends React.Component {
+  state = {
+    listUsers: [
+      { id: 1, name: "Nguyen Quoc Huy", age: "20" },
+      { id: 2, name: "Chip", age: "15" },
+      { id: 3, name: "Nguyen Tien Thanh", age: "50" },
+    ],
+  };
+
+  handleAddNewUser = (userObj) => {
+    this.setState({
+      listUsers: [...this.state.listUsers, userObj],
+    });
+  };
+
   //JSX
   render() {
     return (
       <div>
-        My first Component
-        {Math.random()}
+        <AddUserInfor handleAddNewUser={this.handleAddNewUser} />
+        <br />
+        <DisplayInfor listUsers={this.state.listUsers} />
       </div>
     );
   }
